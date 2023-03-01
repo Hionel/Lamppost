73b34c272871b710d2e886dc789b4837373dda23
Hionel <104563991+Hionel@users.noreply.github.com>
2023-03-01 19:46:24 +0200

    init ng and firebase setup done

---
+++ lamppost-app/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'lamppost-b0a96',
+    appId: '1:98713937489:web:6fdd73ca61841228173a70',
+    storageBucket: 'lamppost-b0a96.appspot.com',
+    apiKey: 'AIzaSyDpLEI3xR0pcZG0A7XbhnI8Z5HkgiMUFLQ',
+    authDomain: 'lamppost-b0a96.firebaseapp.com',
+    messagingSenderId: '98713937489',
+  },
+  prod: false,
+};
