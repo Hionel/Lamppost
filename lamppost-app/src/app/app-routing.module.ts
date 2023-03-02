a66e8a481e7d721646398b5d808d6e0310123c12
Hionel <104563991+Hionel@users.noreply.github.com>
2023-03-03 01:02:25 +0200

    auth-page init setup

--- lamppost-app/src/app/app-routing.module.ts
+++ lamppost-app/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminSignupComponent } from './components/authentication/admin-signup/admin-signup.component';
+import { AuthPageComponent } from './components/authentication/auth-page/auth-page.component';
+import { LoginComponent } from './components/authentication/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'authentication',
+  },
+  {
+    path: 'authentication',
+    component: AuthPageComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'administrator-signup',
+        component: AdminSignupComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- lamppost-app/src/app/app.module.ts
+++ lamppost-app/src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { AppComponent } from './app.component';
+import { AuthPageComponent } from './components/authentication/auth-page/auth-page.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { AdminSignupComponent } from './components/authentication/admin-signup/admin-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPageComponent,
+    LoginComponent,
+    AdminSignupComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
